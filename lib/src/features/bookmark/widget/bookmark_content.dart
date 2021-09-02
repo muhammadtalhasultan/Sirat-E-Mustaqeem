113e6bbac63b1b248a013a1efee9e23b2e8c7a34
Jing En <maliktalha0101@gmail.com>
2021-09-02 19:43:05 +0800

    handle empty bookmark screen

--- lib/src/features/bookmark/widget/bookmark_content.dart
+++ lib/src/features/bookmark/widget/bookmark_content.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lottie/lottie.dart';
+import 'package:sirat_e_mustaqeem/src/core/util/constants.dart';
 
 import '../../utils/loading_widget.dart';
 import '../bloc/category_bloc.dart';
@@ -34,7 +37,29 @@ class _BookmarkContentState extends State<BookmarkContent> {
         if (state is CategoryDone) {
           return SuccessWidget();
         } else {
-          return Text('empty');
+          return SafeArea(
+            child: Center(
+              child: Padding(
+                padding: kPagePadding,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ConstrainedBox(
+                      constraints: BoxConstraints(maxWidth: 500.w),
+                      child: LottieBuilder.asset(
+                        'assets/images/core/json/empty_bookmark.json',
+                      ),
+                    ),
+                    Text(
+                      'You have not favorited or bookmarked any Qurans, Tasbihs or Duas.',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.headline6,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
         }
       },
     );
